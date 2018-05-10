6b2500d30f37ab2a2bc73933f5b67363d9544c3a
dames <dames@pageuppeople.com>
2018-05-11 09:55:37 +1000

    Forcing an error

--- test_full_refresh_from_mssql.cmd
+++ test_full_refresh_from_mssql.cmd
@@ -1,4 +1,4 @@
-py rdl.py mssql+pyodbc://dwsource postgresql+psycopg2://postgres:xxxx@localhost/dest_dw .\integration_tests\mssql_source\config\ --log-level INFO --full-refresh yes
+py rdl.py mssql+pyodbc://dwsource postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/dest_dw .\integration_tests\mssql_source\config\ --log-level INFO --full-refresh yes
 
 
 
--- test_incremental_refresh_from_csv.cmd
+++ test_incremental_refresh_from_csv.cmd
@@ -1,7 +1,7 @@
-py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:password@/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
+py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-py rdl.py csv://.\integration_tests\csv_source\incremental_refresh_data\ postgresql+psycopg2://postgres:xxxx@localhost/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh no
+py rdl.py csv://.\integration_tests\csv_source\incremental_refresh_data\ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh no
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 
