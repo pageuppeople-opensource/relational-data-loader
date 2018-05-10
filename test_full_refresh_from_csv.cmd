29fc5c421cf180459dcc3b0fde02307ff3109a2d
dames <dames@pageuppeople.com>
2018-05-10 14:52:01 +1000

    Added tests

--- test_full_refresh_from_csv.cmd
+++ test_full_refresh_from_csv.cmd
@@ -1,6 +1,6 @@
-py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:password@/dest_dw .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
+py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:password@/relational_data_loader .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
 
-psql -U postgres -d dest_dw -a -f .\integration_tests\csv_source\assertions\column_test_full_refresh_assertions.sql
+psql -U postgres -d relational_data_loader -a -f .\integration_tests\csv_source\assertions\column_test_full_refresh_assertions.sql
 
 
 
