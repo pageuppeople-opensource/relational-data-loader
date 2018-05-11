7abc662e825ef4dd6066cffa769091afe693e013
dames <dames@pageuppeople.com>
2018-05-11 10:12:37 +1000

    Return a specific error code from full load

--- test_full_refresh_from_csv.cmd
+++ test_full_refresh_from_csv.cmd
@@ -1,7 +1,8 @@
 py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
 psql -U postgres -d relational_data_loader_integration_tests -a -f .\integration_tests\csv_source\assertions\column_test_full_refresh_assertions.sql
-if %errorlevel% neq 0 exit /b %errorlevel%
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+EXIT /B 1
 
 
 
