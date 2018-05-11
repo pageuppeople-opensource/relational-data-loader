1a0bfdb75b2582487f5958099ba302d8338a56db
dames <dames@pageuppeople.com>
2018-05-11 13:50:46 +1000

    Starting to hook up mssql source

--- test_full_refresh_from_mssql.cmd
+++ test_full_refresh_from_mssql.cmd
@@ -1,4 +1,9 @@
-py rdl.py mssql+pyodbc://dwsource postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/dest_dw .\integration_tests\mssql_source\config\ --log-level INFO --full-refresh yes
+py rdl.py mssql+pyodbc://(local)\SQL2016/RelationalDataLoaderIntegrationTestSource?driver=SQL+Server+Native+Client+11.0 postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests .\integration_tests\mssql_source\config\ --log-level DEBUG --full-refresh yes
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+
+
+
 
 
 
