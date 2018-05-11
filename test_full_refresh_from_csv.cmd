85b5f2843865fd4d035ba8193a0b0c81fc1ebe85
dames <dames@pageuppeople.com>
2018-05-11 10:19:34 +1000

    echo psql-ErrorLevel %errorlevel%

--- test_full_refresh_from_csv.cmd
+++ test_full_refresh_from_csv.cmd
@@ -1,6 +1,7 @@
 py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
 psql -U postgres -d relational_data_loader_integration_tests -a -f .\integration_tests\csv_source\assertions\column_test_full_refresh_assertions.sql
+echo psql-ErrorLevel %errorlevel%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 
