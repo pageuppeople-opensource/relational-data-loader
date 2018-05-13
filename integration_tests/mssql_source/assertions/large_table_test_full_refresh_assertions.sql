768d5b3193a9130cbd3c411575860571c76ce2c2
dames <dames@pageuppeople.com>
2018-05-14 08:44:33 +1000

    This should fail.

---
+++ integration_tests/mssql_source/assertions/large_table_test_full_refresh_assertions.sql
@@ -0,0 +1,13 @@
+SET client_encoding TO 'UTF8';
+
+
+do $$
+  BEGIN
+  IF (SELECT count(*) FROM rdl_integration_tests.load_large_data ) = 100000 THEN
+    RAISE NOTICE '[LARGE MSSQL IMPORT TEST] PASS';
+  ELSE
+    RAISE EXCEPTION '[LARGE MSSQL IMPORT TEST] FAIL: Did not find the required 1,000,000 rows.';
+  END IF;
+END $$;
+
+
