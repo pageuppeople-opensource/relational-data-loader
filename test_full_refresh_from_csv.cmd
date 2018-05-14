9c109715182728e922a7aa0ecd03874e827cca09
dames <dames@pageuppeople.com>
2018-05-15 08:43:51 +1000

    Trying to swap slashes

--- test_full_refresh_from_csv.cmd
+++ test_full_refresh_from_csv.cmd
@@ -1,6 +1,6 @@
-py rdl.py csv://.\integration_tests\csv_source\full_refresh_data\ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests .\integration_tests\csv_source\config\ --log-level INFO --full-refresh yes
+py rdl.py csv://./integration_tests/csv_source/full_refresh_data/ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests ./integration_tests/csv_source/config/ --log-level INFO --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
-psql -U postgres -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f .\integration_tests\csv_source\assertions\column_test_full_refresh_assertions.sql
+psql -U postgres -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f ./integration_tests/csv_source/assertions/column_test_full_refresh_assertions.sql
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 
--- test_full_refresh_from_mssql.cmd
+++ test_full_refresh_from_mssql.cmd
@@ -1,8 +1,8 @@
-IF /I "%APPVEYOR%"=="TRUE" py rdl.py mssql+pyodbc://(local)\SQL2016/RelationalDataLoaderIntegrationTestSource?driver=SQL+Server+Native+Client+11.0 postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests .\integration_tests\mssql_source\config\ --log-level DEBUG --full-refresh yes
+IF /I "%APPVEYOR%"=="TRUE" py rdl.py mssql+pyodbc://(local)\SQL2016/RelationalDataLoaderIntegrationTestSource?driver=SQL+Server+Native+Client+11.0 postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests ./integration_tests/mssql_source/config/ --log-level DEBUG --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
-IF /I NOT "%APPVEYOR%"=="TRUE" py rdl.py mssql+pyodbc://(local)/RelationalDataLoaderIntegrationTestSource?driver=SQL+Server+Native+Client+11.0 postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests .\integration_tests\mssql_source\config\ --log-level DEBUG --full-refresh yes
+IF /I NOT "%APPVEYOR%"=="TRUE" py rdl.py mssql+pyodbc://(local)/RelationalDataLoaderIntegrationTestSource?driver=SQL+Server+Native+Client+11.0 postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests ./integration_tests/mssql_source/config/ --log-level DEBUG --full-refresh yes
 if %errorlevel% neq 0 exit /b %errorlevel%
-psql -U postgres -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f .\integration_tests\mssql_source\assertions\large_table_test_full_refresh_assertions.sql
+psql -U postgres -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f ./integration_tests/mssql_source/assertions/large_table_test_full_refresh_assertions.sql
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 
