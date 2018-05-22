9bf881df02c37c45b691fe922733f670cc653d02
dames <dames@pageuppeople.com>
2018-05-23 07:51:17 +1000

    Fixed tests for Bigint

--- integration_tests/csv_source/assertions/column_test_full_refresh_assertions.sql
+++ integration_tests/csv_source/assertions/column_test_full_refresh_assertions.sql
@@ -3,20 +3,20 @@ SET client_encoding TO 'UTF8';
 DROP TABLE IF EXISTS results;
 
 CREATE TEMPORARY TABLE results AS
-  WITH expected(id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1, guid_column_1) AS (
-    SELECT 1, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String', '57bc8093-fe4c-477a-bbd7-fb5c02055a7e'::UUID
+  WITH expected(id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1, guid_column_1,big_int_column_1) AS (
+    SELECT 1, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String', '57bc8093-fe4c-477a-bbd7-fb5c02055a7e'::UUID,2147483647121212
     UNION ALL
-    SELECT 2, NULL, NULL, NULL, NULL, NULL, NULL
+    SELECT 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL
     UNION ALL
-    SELECT 3, 333.0, '2001-01-01', 33.333, NULL, 'This Text Has a Quote Before "Dave', NULL
+    SELECT 3, 333.0, '2001-01-01', 33.333, NULL, 'This Text Has a Quote Before "Dave', NULL, NULL
     UNION ALL
-    SELECT 4, NULL, NULL, NULL, NULL, 'ം ഃ അ ആ ഇ ഈ ഉ ഊ ഋ ഌ എ ഏ','aabc8093-fe4c-477a-bbd7-fb5c02055a7e'
+    SELECT 4, NULL, NULL, NULL, NULL, 'ം ഃ അ ആ ഇ ഈ ഉ ഊ ഋ ഌ എ ഏ','aabc8093-fe4c-477a-bbd7-fb5c02055a7e', NULL
     UNION ALL
-    SELECT 5, NULL, NULL, NULL, NULL, 'This row will be updated in the incremental review test', NULL
+    SELECT 5, NULL, NULL, NULL, NULL, 'This row will be updated in the incremental review test', NULL, NULL
   ),
 
   actual AS (
-    SELECT  id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1,guid_column_1
+    SELECT  id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1,guid_column_1,big_int_column_1
     FROM    rdl_integration_tests.load_source_data
   )
 
