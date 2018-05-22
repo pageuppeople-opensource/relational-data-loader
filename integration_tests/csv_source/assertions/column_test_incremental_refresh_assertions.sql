ebd2ee61b3ad58dbbcf8ef17706fa2a9d4dff0e6
dames <dames@pageuppeople.com>
2018-05-22 10:52:58 +1000

    Adding guid to tests

--- integration_tests/csv_source/assertions/column_test_incremental_refresh_assertions.sql
+++ integration_tests/csv_source/assertions/column_test_incremental_refresh_assertions.sql
@@ -2,24 +2,24 @@ SET client_encoding TO 'UTF8';
 DROP TABLE IF EXISTS results;
 
 CREATE TEMPORARY TABLE results AS
-  WITH expected(id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1) AS (
-    SELECT 1, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String'
+  WITH expected(id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1, guid_column_1) AS (
+    SELECT 1, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String', '57bc8093-fe4c-477a-bbd7-fb5c02055a7e'::UUID
     UNION ALL
-    SELECT 2, NULL, NULL, NULL, NULL, NULL
+    SELECT 2, NULL, NULL, NULL, NULL, NULL, NULL
     UNION ALL
-    SELECT 3, 333.0, '2001-01-01', 33.333, NULL, 'This Text Has a Quote Before "Dave'
+    SELECT 3, 333.0, '2001-01-01', 33.333, NULL, 'This Text Has a Quote Before "Dave', NULL
     UNION ALL
-    SELECT 4, NULL, NULL, NULL, NULL, 'ം ഃ അ ആ ഇ ഈ ഉ ഊ ഋ ഌ എ ഏ'
+    SELECT 4, NULL, NULL, NULL, NULL, 'ം ഃ അ ആ ഇ ഈ ഉ ഊ ഋ ഌ എ ഏ', 'aabc8093-fe4c-477a-bbd7-fb5c02055a7e'
     UNION ALL
-    SELECT 5, NULL, NULL, NULL, NULL, 'This row WAS updated in the incremental review test'
+    SELECT 5, NULL, NULL, NULL, NULL, 'This row WAS updated in the incremental review test', NULL
     UNION ALL
-    SELECT 6, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String'
+    SELECT 6, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'A Basic String', '57bc8093-fe4c-477a-bbd7-fb5c02055a7e'
     UNION ALL
-    SELECT 7, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'Another Basic String'
+    SELECT 7, 111.0, '1976-12-01'::DATE, 12.1212, '1976-12-01 01:00:00.000000'::TIMESTAMP, 'Another Basic String', NULL
   ),
 
   actual AS (
-    SELECT  id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1
+    SELECT  id, int_column_1, date_column_1, decimal_column_1, date_time_column_1, string_column_1, guid_column_1
     FROM    rdl_integration_tests.load_source_data
   )
 
