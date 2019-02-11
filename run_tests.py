fb147e78f7b7effe2d4e5677cdce6b3b0f92ab72
Sean Budd <seanb@pageuppeople.com>
2019-02-11 14:58:29 +1100

    add reference to unit test information

--- run_tests.py
+++ run_tests.py
@@ -2,4 +2,5 @@ from modules.tests import test_MsSqlDataSource
 import unittest
 
 suite = unittest.TestLoader().loadTestsFromModule(test_MsSqlDataSource)
+# https://stackoverflow.com/a/1322648/8030743
 unittest.TextTestRunner(verbosity=2).run(suite)
