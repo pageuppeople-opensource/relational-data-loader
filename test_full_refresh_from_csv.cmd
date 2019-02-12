0db64b48c16e7870ab1680b6ca1d4679583d39c4
Sean Budd <seanb@pageuppeople.com>
2019-02-12 15:16:03 +1100

    [OSC-1223] update integration testing formatting

--- test_full_refresh_from_csv.cmd
+++ test_full_refresh_from_csv.cmd
@@ -1,11 +1,5 @@
 py rdl.py csv://./integration_tests/csv_source/full_refresh_data/ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@/relational_data_loader_integration_tests ./integration_tests/csv_source/config/ --log-level INFO --force-full-refresh-models *
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 psql -U postgres -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f ./integration_tests/csv_source/assertions/column_test_full_refresh_assertions.sql
 if %errorlevel% neq 0 exit /b %errorlevel%
-
-
-
-
-
-
-
