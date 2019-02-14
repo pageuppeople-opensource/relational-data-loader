c2fed4b0ff497b64de79e2fb19779bca126cec1a
Sean Budd <seanb@pageuppeople.com>
2019-02-15 10:48:13 +1100

    update integration tests [wip]

--- integration_tests/mssql_source/source_database_setup/change_large_table.sql
+++ integration_tests/mssql_source/source_database_setup/change_large_table.sql
@@ -16,13 +16,13 @@ INSERT LargeTable
         GuidColumn,
         BoolColumn
         )
-SELECT n,
-        CASE WHEN n % 3 = 0 THEN NULL ELSE 'Row Number ' + CAST(n as varchar) END,
-        CASE WHEN n % 5 = 0 THEN NULL ELSE DateAdd(hour, -n, '2000-01-1') END,
-        CASE WHEN n % 7 = 0 THEN NULL ELSE DateAdd(hour, n, '2000-01-1') END,
-        CASE WHEN n % 9 = 0 THEN NULL ELSE n * 1000 END,
-        CASE WHEN n % 11 = 0 THEN NULL ELSE N'काचं शक्नोम्यत्तुम् । नोपहिनस्ति माम् ॥' END,
-        CASE WHEN n % 13 = 0 THEN NULL ELSE newid() END,
-        CASE WHEN n % 3 = 0 THEN NULL ELSE 1 END
+SELECT 999999999999999,
+        'test change',
+        '1999-01-1',
+        '1999-02-1',
+        -2,
+        'foobar',
+        newid(),
+        1
 
 FROM Numbers
