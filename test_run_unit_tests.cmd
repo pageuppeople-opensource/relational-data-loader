2da2f9ede1bcd273b317ef61f1bc1643a6604117
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-02-15 12:25:24 +1100

    testing unit test failure

--- test_run_unit_tests.cmd
+++ test_run_unit_tests.cmd
@@ -2,4 +2,8 @@ chcp 65001
 echo {"username": "","password": "","server_string": "(local)\\SQL2016"} > .\modules\tests\config\connection.json
 
 py ./run_tests.py
+if ERRORLEVEL 0 echo Does zero print?
+if ERRORLEVEL 1 echo Does one print?
+if %ERRORLEVEL% 0 echo Does percentage zero print?
+if %ERRORLEVEL% 1 echo Does percentage one print?
 if %errorlevel% neq 0 exit /b %errorlevel%
