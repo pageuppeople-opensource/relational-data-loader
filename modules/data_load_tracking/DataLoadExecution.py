e30d0e02fe47a3bbd44d0aac4bd70fe9e97e0d0e
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-02-18 11:57:19 +1100

    (a) use UUID type for correlation_id column where UUID is stored (b) reorder data_load_execution columns to group similar columns together

--- modules/data_load_tracking/DataLoadExecution.py
+++ modules/data_load_tracking/DataLoadExecution.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, DateTime, Integer, String, Boolean, BigInteger
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
 
 Base = declarative_base()
 
@@ -9,14 +10,14 @@ class DataLoadExecution(Base):
     __tablename__ = 'data_load_execution'
     __table_args__ = {'schema': 'data_pipeline'}
     id = Column(Integer, primary_key=True)
+    correlation_id = Column(UUID(as_uuid=True), nullable=True)
     model_name = Column(String(250), nullable=False)
-    is_full_refresh = Column(Boolean, nullable=False)
+    status = Column(String(25), nullable=False)
     this_sync_version = Column(BigInteger, nullable=False)
     next_sync_version = Column(BigInteger, nullable=False)
+    is_full_refresh = Column(Boolean, nullable=False)
+    full_refresh_reason = Column(String(100), nullable=False)
     completed_on = Column(DateTime(timezone=True), server_default=func.now())
     execution_time_ms = Column(Integer, nullable=False)
     rows_processed = Column(Integer, nullable=False)
-    correlation_id = Column(String(250), nullable=True)
-    status = Column(String(25), nullable=False)
     model_checksum = Column(String(100), nullable=False)
-    full_refresh_reason = Column(String(100), nullable=False)
