c0df9a7d8caa17a9c3d0a7dff115d2b15d7b8a0a
Sean Budd <seanb@pageuppeople.com>
2019-03-04 12:52:09 +1100

    update unit tests and read me for module

--- test_incremental_refresh_from_csv.cmd
+++ test_incremental_refresh_from_csv.cmd
@@ -1,4 +1,4 @@
-py rdl.py process csv://./integration_tests/csv_source/incremental_refresh_data/ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests ./integration_tests/csv_source/config/ --log-level INFO
+py -m rdl process csv://./integration_tests/csv_source/incremental_refresh_data/ postgresql+psycopg2://postgres:there_is_no_password_due_to_pg_trust@localhost/relational_data_loader_integration_tests ./integration_tests/csv_source/config/ --log-level INFO
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 psql -U postgres -q -d relational_data_loader_integration_tests -a -v ON_ERROR_STOP=1 -f ./integration_tests/csv_source\assertions/column_test_incremental_refresh_assertions.sql
