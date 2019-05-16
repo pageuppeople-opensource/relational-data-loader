860f27f782b59cc8182b3feed667c9b5d5af7b5f
Chintan Raval <ChintanRaval@users.noreply.github.com>
2019-05-16 14:12:32 +1000

    run unit tests with elevated user as the tests setup new database

--- tests/unit_tests/run_unit_tests.cmd
+++ tests/unit_tests/run_unit_tests.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-echo {"mssql": {"username": "","password": "","server_string": "(local)\\SQL2016"}, "psql": {"username": "rdl_integration_test_user","password": "rdl_integration_test_password","server_string": "localhost"}}> .\tests\unit_tests\config\connection.json
+echo {"mssql": {"username": "","password": "","server_string": "(local)\\SQL2016"}, "psql": {"username": "postgres","password": "there_is_no_password_due_to_pg_trust","server_string": "localhost"}}> .\tests\unit_tests\config\connection.json
 
 py ./tests/unit_tests/main.py
 if %errorlevel% neq 0 exit /b %errorlevel%
