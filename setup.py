a640d4bbc941295d4cb94fbe5611b4fd361b0079
Ben Edwards <bene@pageuppeople.com>
2019-11-12 10:09:35 +1100

    support psycopg with libpq compatibility

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup, find_packages
+import os
+import sys
+
+def _rdl_psycopg2_name():
+    # if the user chose something, use that
+    # * If you cannot install a library containing pg_config, set the environemnt variable RDL_PSYCOPG2_NAME='psycopg2-binary'
+    # * Reference: https://stackoverflow.com/questions/11618898/pg-config-executable-not-found/12037133#12037133
+    package_name = os.getenv('RDL_PSYCOPG2_NAME', '')
+    if package_name:
+        return package_name
+
+    binary_only_versions = [(3, 8)]
+
+    # binary wheels don't exist for all versions. Require psycopg2-binary for
+    # them and wait for psycopg2.
+    if sys.version_info[:2] in binary_only_versions:
+        return 'psycopg2-binary'
+    else:
+        return 'psycopg2'
+
+RDL_PSYCOPG2_NAME = _rdl_psycopg2_name()
 
 setup(
     name="rdl",
@@ -7,7 +28,7 @@ setup(
     install_requires=[
         "numpy==1.16.2",
         "pandas==0.24.2",
-        "psycopg2-binary==2.8.2",
+        '{}~=2.8'.format(RDL_PSYCOPG2_NAME),
         "pyodbc==4.0.26",
         "python-dateutil==2.8.0",
         "pytz==2019.1",
